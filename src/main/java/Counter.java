8e32a72365aed906b2e28e8733f7e92c727a5c32
spiderrooster <louisthorne12@gmail.com>
2022-05-04 23:23:58 +0100

    WordCounterApi inital commit. Basics plotted word counter can trim, split,calculate average, max and min

---
+++ src/main/java/Counter.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Counter {
+    int length = 0;
+    int tally = 0;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getTally() {
+        return tally;
+    }
+
+    public void setTally(int tally) {
+        this.tally = tally;
+    }
+}
