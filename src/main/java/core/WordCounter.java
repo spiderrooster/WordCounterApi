dd8bd37b178514ccf523ff55e38d1dd46fe686ca
Your Name <louisthorne12@gmail.com>
2022-05-05 00:40:57 +0100

    implemented input stream and buffered reader so large blocks of data can be read

--- src/main/java/core/WordCounter.java
+++ src/main/java/core/WordCounter.java
@@ -1,8 +1,17 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WordCounter {
+
+    long wordSum = 0;
+    long wordTally = 0;
+    double average = 0.0;
 
     public static void main(String[] args) {
         WordCounter wordCounter = new WordCounter();
@@ -10,7 +19,7 @@ public class WordCounter {
         String result = wordCounter.trimString("hello   hello    hello");
         System.out.println(result);
 
-        double avergae = wordCounter.averageLength(Arrays.asList("12", "hello", "hello"));
+        double avergae = wordCounter.averageLengthList(Arrays.asList("12", "hello", "hello"));
         System.out.println(avergae);
 
         int max = wordCounter.maxLength(Arrays.asList("12", "12345", "123456789"));
@@ -22,21 +31,42 @@ public class WordCounter {
         int total = 0;
 
         String trimmed = wordCounter.trimString("Hello world & good morning. The date is 18/05/2016");
-        List<String> split = wordCounter.splitString(trimmed);
+        List<String> split = wordCounter.splitString(trimmed, " ");
         total += split.size();
         System.out.println("Word Count = " + total);
         System.out.println("Word Count = " + total);
+    }
+
+    public void readInputStream(InputStream inputStream) {
+        wordSum = 0;
+        wordTally = 0;
+        average = 0.0;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            while (reader.ready()) {
+                String trimmedLine = trimString(reader.readLine());
+                List<String> split = splitString(trimmedLine, " ");
+                for (String value : split) {
+                    wordSum += value.length();
+                }
+                wordTally += split.size();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        average = (double) wordSum / (double) wordTally;
     }
 
     public String trimString(String toTrim) {
         return toTrim.replaceAll("\\s+", " ");
     }
 
-    public List<String> splitString(String toSplit) {
-        return Arrays.asList(toSplit.split(" "));
+    public List<String> splitString(String toSplit, String regex) {
+        return Arrays.asList(toSplit.split(regex));
     }
 
-    public double averageLength(List<String> listToAverage) {
+    public double averageLengthList(List<String> listToAverage) {
         return listToAverage.stream().map(String::length).mapToDouble(l -> l).average().orElse(0.0);
     }
 
